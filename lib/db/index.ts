6504930c025fe6752c17ebe5bc138cfa86c9b086
FTXFabrizio <fabrizioftx@gmail.com>
2026-03-04 10:20:06 -0500

    feat improvement index

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,22 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
+import { createClient, Client } from "@libsql/client";
 import * as schema from "./schema";
 
-const client = createClient({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_TOKEN,
-});
+const createDbClient = () => {
+  return createClient({
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_TOKEN,
+  });
+};
+
+const globalForDb = globalThis as unknown as {
+  client: Client | undefined;
+};
+
+const client = globalForDb.client ?? createDbClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForDb.client = client;
+}
 
 export const db = drizzle(client, { schema });
